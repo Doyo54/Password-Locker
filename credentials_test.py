41baa456eeafb61bc0a0e1d0c27eef20ae614a73
Doyo Abdikadir <doyo.doyo@student.moringaschool.com>
2022-04-26 12:25:18 +0300

    ft:Test_init added to test if the object is initialized properly

--- credentials_test.py
+++ credentials_test.py
@@ -1,1 +1,22 @@
 import unittest
+from credentials import Credentials
+
+class testCredentials(unittest.TestCase):
+     
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_credentials = Credentials("Instagram", "Josh","brian") # create credentials object
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_credentials.credential_name,"Instagram")
+        self.assertEqual(self.new_credentials.credential_user_name,"Josh")
+        self.assertEqual(self.new_credentials.credential_password,"brian")
+
+if __name__ == '__main__':
+    unittest.main()        
